7cb0b78e349217ffb67c2cf84b39f6c910b7ab50
Mario <mario.saliba98@gmail.com>
2017-10-08 23:23:29 +0300

    build and style thje footer page

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="tc bg-pink pt4">
+    <span className="f2">Shireen Zoaby &copy;</span>
+  </div>
+);
+
+export default Footer;
